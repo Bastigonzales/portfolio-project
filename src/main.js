860e5a969a09218d4fe043067e211d8ae1d25bf8
Basti Gonzales <bastigonzales@protonmail.com>
2023-09-28 03:08:24 +0800

    import about css

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/profile.css";
+import "../styles/components/about.css";
 import "../styles/utils.css";
