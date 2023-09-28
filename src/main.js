13b5c6480071887b9397bb90dc51c7c4fd776087
Basti Gonzales <bastigonzales@protonmail.com>
2023-09-28 19:00:36 +0800

    import project css

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/profile.css";
 import "../styles/components/about.css";
+import "../styles/components/project.css";
+import "../styles/components/contact.css";
 import "../styles/utils.css";
